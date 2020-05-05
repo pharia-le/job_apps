f3e5d6a4d2b67aa92a542a45562d71c22a29c509
pharia-le <pharia.le@gmail.com>
2020-05-05 02:58:58 -0700

    Add helper methods to app controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,27 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        flash[:errors] = "You must be logged in to view the page you tried to view."
+        redirect "/"
+      end
+    end
+
+    def redirect_if_logged_in
+      if logged_in?
+        redirect '/users/home'
+      end
+    end
+  end
+  
 end
